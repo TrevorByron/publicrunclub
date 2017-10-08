6ec4d6587e1c1698131624e8a31f0efc503f51a6
Trevor Borden <tborden@Trevors-MacBook-Pro-2.local>
2017-10-08 15:06:31 -0500

    figuring out the timeline

--- js/main.js
+++ js/main.js
@@ -1,28 +1,15 @@
-// Smooth Scroll
+// SMOOTH SCROLLING SECTIONS
+$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
 
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+        }
     }
-  });
 });
-
-// hamburger
-
-(function(){
-
-  $('.hamburger').on('click', function(){
-
-    $(this).toggleClass('is-active');
-
-  })
-
-}());
